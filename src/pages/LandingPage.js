3a7c0e4c9cf2d05812b15ef86c29e07f341a9a81
Joshua Nathaniel M <clsjoshua@gmail.com>
2020-05-17 14:34:41 +0700

    add categories at landingpage

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,16 +1,25 @@
 import landingPage from 'json/landingPage.json'
+import Categories from 'parts/Categories'
 import Header from 'parts/Header'
 import Hero from 'parts/Hero'
 import MostPicked from 'parts/MostPicked'
 import React, { Component } from 'react'
 
 export default class LandingPage extends Component {
+  constructor(props) {
+    super(props)
+    this.refMostPicked = React.createRef()
+  }
   render() {
     return (
       <>
         <Header {...this.props} />
-        <Hero data={landingPage.hero} />
-        <MostPicked data={landingPage.mostPicked} />
+        <Hero data={landingPage.hero} refMostPicked={this.refMostPicked} />
+        <MostPicked
+          refMostPicked={this.refMostPicked}
+          data={landingPage.mostPicked}
+        />
+        <Categories data={landingPage.categories} />
       </>
     )
   }
