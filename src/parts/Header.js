308533d4faae370a45ebfefc15675115d4e27d72
Joshua Nathaniel M <clsjoshua@gmail.com>
2020-05-20 18:08:40 +0700

    add landingpage animations

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,6 +1,7 @@
 import Button from 'elements/Button'
 import BrandIcon from 'parts/IconText'
 import React from 'react'
+import Fade from 'react-reveal/Fade'
 
 export default function Header(props) {
   const getNavLinkClass = (path) => {
@@ -12,7 +13,7 @@ export default function Header(props) {
       <header className='spacing-sm'>
         <div className='container'>
           <nav className='navbar navbar-expand-lg navbar-light'>
-            <Button className='brand-text-icon mx-auto' href='' type='link'>
+            <Button className='mx-auto brand-text-icon' href='' type='link'>
               Stay<span className='text-gray-900'>cation.</span>
             </Button>
           </nav>
@@ -21,36 +22,38 @@ export default function Header(props) {
     )
 
   return (
-    <header className='spacing-sm'>
-      <div className='container'>
-        <nav className='navbar navbar-expand-lg navbar-light'>
-          <BrandIcon />
-          <div className='collapse navbar-collapse'>
-            <ul className='navbar-nav ml-auto'>
-              <li className={`nav-item${getNavLinkClass('/')}`}>
-                <Button className='nav-link' type='link' href='/'>
-                  Home
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass('/browse-by')}`}>
-                <Button className='nav-link' type='link' href='/browse-by'>
-                  Browse By
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass('/stories')}`}>
-                <Button className='nav-link' type='link' href='/stories'>
-                  Stories
-                </Button>
-              </li>
-              <li className={`nav-item${getNavLinkClass('/agents')}`}>
-                <Button className='nav-link' type='link' href='/agents'>
-                  Agents
-                </Button>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <Fade top>
+      <header className='spacing-sm'>
+        <div className='container'>
+          <nav className='navbar navbar-expand-lg navbar-light'>
+            <BrandIcon />
+            <div className='collapse navbar-collapse'>
+              <ul className='ml-auto navbar-nav'>
+                <li className={`nav-item${getNavLinkClass('/')}`}>
+                  <Button className='nav-link' type='link' href='/'>
+                    Home
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass('/browse-by')}`}>
+                  <Button className='nav-link' type='link' href='/browse-by'>
+                    Browse By
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass('/stories')}`}>
+                  <Button className='nav-link' type='link' href='/stories'>
+                    Stories
+                  </Button>
+                </li>
+                <li className={`nav-item${getNavLinkClass('/agents')}`}>
+                  <Button className='nav-link' type='link' href='/agents'>
+                    Agents
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </header>
+    </Fade>
   )
 }
