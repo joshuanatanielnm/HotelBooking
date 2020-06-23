08d4ac2f397f132316ba746adff31d7fc376a2f7
Joshua Nathaniel M <clsjoshua@gmail.com>
2020-06-23 10:56:50 +0700

    add input and icons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './assets/scss/style.scss'
 
-import LandingPage from 'pages/LandingPage'
+import Example from 'pages/Example'
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+// import LandingPage from 'pages/LandingPage'
 function App() {
   return (
     <div className='App'>
       <Router>
-        <Route path='/' component={LandingPage}></Route>
+        <Route path='/' component={Example}></Route>
       </Router>
     </div>
   )
